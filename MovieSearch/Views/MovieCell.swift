d04ed9549f376b00d757794b96b647bbe3bca812
Jacob Bullock <jacob.bullock@gmail.com>
2019-09-06 23:00:13 -0600

    add configure method for displaying movie

--- MovieSearch/Views/MovieCell.swift
+++ MovieSearch/Views/MovieCell.swift
@@ -13,4 +13,20 @@ class MovieCell: UITableViewCell {
     @IBOutlet var posterImageView: LoadingImageView!
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var overviewLabel: UILabel!
+
+    public func configure(movie: Movie) {
+        titleLabel.text = movie.title
+        overviewLabel.text = movie.overview
+        if let poster = movie.posterPath,
+            let url = URL(string: "\(MovieService.basePosterURL)\(poster)") {
+            posterImageView.load(url: url)
+        } else {
+            // TODO: add placeholder image per challenge instructions
+            posterImageView.image = nil
+        }
+
+        isAccessibilityElement = true
+        accessibilityLabel = movie.title
+        accessibilityHint = movie.overview
+    }
 }
